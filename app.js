ede0f4b3527d9c0632e62a3bf9d31f889f1216fe
Treasure <treasure.rose@gmail.com>
2018-04-17 07:58:18 -0700

    code review changes

--- app.js
+++ app.js
@@ -7,50 +7,50 @@ const app = new Vue({
       {
         title: 'Hop on Pop', 
         description: "A delightful children's book.",
-        tag: 'book',
+        type: 'book',
         contributor: 'Dr. Suess',
         showDetail: false,
       },
       {
         title: 'The Joy of Painting', 
         description: "Create a world of happy little trees!",
-        tag: 'video',
+        type: 'DVD',
         contributor: 'Bob Ross',
         showDetail: false
       },
       {
         title: 'Supernatural: The Complete 12th Season', 
         description: "A (literally) neverending roadtrip.",
-        tag: 'video',
-        contributor: "Eric Kripke",
+        type: 'DVD',
+        contributor: "",
         showDetail: false
       },
       {
         title: 'Planet Earth II', 
         description: "Hours of beautiful but heart attack-inducing nature footage",
-        tag: 'streaming video',
+        type: 'streaming video',
         contributor: 'David Attenborough',
         showDetail: false,
       },
       {
         title: 'Titanic', 
         description: "The boat sinks.",
-        tag: 'video',
+        type: 'DVD',
         contributor: 'James Cameron',
         showDetail: false,
       },
       {
         title: 'The Sirens of Titan', 
         description: "Mankind flung its advance agents ever outward, ever outward... it flung them like stones.",
-        tag: 'book',
+        type: 'book',
         contributor: 'Kurt Vonnegut',
         showDetail: false,
       },
       {
         title: 'Better Call Saul', 
         description: "A slow-burning Breaking Bad prequel.",
-        tag: 'streaming video',
-        contributor: 'Vince Gilligan',
+        type: 'streaming video',
+        contributor: '',
         showDetail: false,
       }
     ]
@@ -65,13 +65,13 @@ const app = new Vue({
   }, 
   computed: {
     uniqueItemsList: function(){
-      const tags = [];
+      const types = [];
       this.mediaList.forEach((item) => {
-        if (!tags.includes(item.tag)){
-          tags.push(item.tag);
+        if (!types.includes(item.type)){
+          types.push(item.type);
         }
       });
-      return tags;
+      return types;
     }
   }
 });
